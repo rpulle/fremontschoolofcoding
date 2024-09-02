a8752b0bfbcd75f6cf12b58e771ec1347872e472
rpulle11 <95676247+rpulle11@users.noreply.github.com>
2024-09-02 12:07:18 -0700

    add

--- src/components/HomeInfo.jsx
+++ src/components/HomeInfo.jsx
@@ -3,8 +3,12 @@ import React from 'react'
 const HomeInfo = () => {
   return (
     <div>
-        <h1 className='text-center pt-20 font-bold text-6xl'>{'</ Fremont School of Coding >'}</h1>
-
+        <h1
+          className='text-center pt-20 font-bold text-6xl text-'
+          style={{ fontFamily: 'Poppins, sans-serif' , color: '#273c60'}}
+        >
+          {'</ Fremont School of Coding >'}
+        </h1>
     </div>
   )
 }
